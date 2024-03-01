d17a5bb6c8131801c2bcdb60280c71efdec283ab
Akshat Mittal <akshatmittal2506@gmail.com>
2024-03-01 11:20:41 +0530

    added http utility

--- server.py
+++ server.py
@@ -1,14 +1,19 @@
 from typing import Union
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Request, Response, Body, status
 from fastapi.encoders import jsonable_encoder
 from GenAI import GenerativeAI
+from utils import HTTP
+
 app = FastAPI()
 
 
 @app.post("/generate")
-async def genCommand(body = Body(...)):
-    body=jsonable_encoder(body)
-    print(body,type(body))
+async def genCommand(request: Request, response: Response, body = Body(...)):
+    http = HTTP(response)
+    body = jsonable_encoder(body)
+    print(body, type(body))
+    if "input" not in body:
+        return http.response(status.HTTP_400_BAD_REQUEST, "input is required")
     query = body["input"]
     generativeAI = GenerativeAI()
     # re = str(line)
